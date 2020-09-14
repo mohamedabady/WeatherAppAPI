061badfcf6739ea98abd179df178232f4c42ed03
Mohamed Abady <mabady@tele-ware.com>
2020-09-14 21:01:22 +0200

    finished project

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const listenCallback = () => {
+    console.log('server running on port : ' + port);
+}
+app.listen(port, listenCallback)
+
+// Callback function to complete GET '/getData'
+const getDataCallBack = (request, response) => {
+    response.send(projectData);
+}
+
+// Initialize getData route with a callback function
+app.get('/getData', getDataCallBack);
+
+// Post Route
+app.post('/postData', postDataCallBack);
+
+//Callback function to complete POST './postData'
+function postDataCallBack (request, response) {
+    const { temperature, date, userResponse } = request.body;
+    projectData.temperature = temperature;
+    projectData.date = date;
+    projectData.userResponse = userResponse;
+
+    response.send(JSON.stringify(projectData));
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,71 @@
 /* Global Variables */
-
+const apiKey = '6e832e606f220e244ecd51c5d1cac56b';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// create event listener for generate button
+document.getElementById('generate').addEventListener('click', () => getWeatherData(apiKey));
+
+// ----------------------- functions creations -----------------------
+// Post data function
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+// Update UI 
+const updateUI = async () => {
+    const request = await fetch('/getData');
+    try {
+        const projectData = await request.json();
+        document.getElementById('date').innerHTML = projectData.date;
+        document.getElementById('temp').innerHTML = projectData.temperature;
+        document.getElementById('content').innerHTML = projectData.userResponse;
+    } catch (e) {
+        console.log("error", error);
+    }
+}
+
+// Create get function to get weather data 
+const getWeatherData = async (apiKeyValue) => {
+    let zipCodeValue = document.getElementById('zip').value;
+    let userResponse = document.getElementById('feelings').value;
+
+    if (zipCodeValue === null || zipCodeValue === '' || zipCodeValue === undefined) {
+        alert('please enter correct zip code of city!!!');
+    } else if (userResponse === null || userResponse === '' || userResponse === undefined) {
+        alert('please enter what you are feeling correctly please!!!');
+    } else {
+        await fetch(`${baseUrl}?zip=${zipCodeValue}&appid=${apiKeyValue}`).then(async (response) => {
+            const res = await response.json();
+
+            const dataForPostReq = {
+                temperature: res.main.temp,
+                date: newDate,
+                userResponse: userResponse
+            };
+
+
+            await postData('/postData', dataForPostReq).then(res => {
+                updateUI();
+            })
+        }).catch(e => {
+            console.log('error : ' + e)
+        })
+    }
+}
